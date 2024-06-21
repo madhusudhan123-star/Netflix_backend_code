b6a6d0bb0a5b82fbc03b037686283146bdb2c318
Madhusudhan <dmadhusudhan98@gmail.com>
2024-06-21 21:05:36 +0530

    code

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use('/api/tvshow', tv_ShowRouter)
 app.use('/api', search) // ?query=your-name
 
 
-app.listen(process.env.PORT ||3001, () => {
+app.listen(process.env.PORT ||3001,'0.0.0.0', () => {
     console.log(`Server is running on port ${process.env.PORT ||3001}`);
 });
 
