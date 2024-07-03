6f01efd16fb06f2ac1b76a5a207aed41d8338681
Madhusudhan <dmadhusudhan98@gmail.com>
2024-07-03 18:19:51 +0530

    changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -35,7 +35,7 @@ const sendEmail = async (to, subject, text, html) => {
 };
 
 const signup = async (req, res) => {
-  const {name, email, password} = req.body;
+  const { name, email, password } = req.body;
 
   try {
     const existingUser = await User.findOne({ email });
@@ -43,15 +43,23 @@ const signup = async (req, res) => {
       return res.status(400).json({ message: 'Email already exists' });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({name, email, password: hashedPassword});
+    const newUser = new User({ name, email, password: hashedPassword });
     await newUser.save();
 
-    // Send welcome email
+    const socialmedia = 'https://www.linkedin.com/in/madhu-sudhan-232089220/';
+    const website = 'https://netflix-front-end-seven.vercel.app/';
     await sendEmail(
       email,
-      'Welcome to Our Website!',
-      `Dear ${name},\n\nWelcome to our website! We're thrilled to have you join our community...`,
-      `<h1>Welcome to Our Website!</h1><p>Dear ${name},</p>...`
+      `Welcome to ${name}!`,
+      `Dear ${name},\n\nWe are thrilled to have you as part of our community. Whether you are a movie buff, a TV series enthusiast, or just looking for something new to watch, we've got you covered.\n\nHere’s what you can do next:\n1. Set Up Your Profile: Customize your profile by uploading a picture and setting a display name.\n2. Choose Your Preferences: Select your favorite genres and actors to help us recommend the best content for you.\n3. Explore Our Library: Dive into our extensive collection of movies, TV shows, and documentaries.\n\nTips & Tricks:\n- Create Multiple Profiles: Share your account with family or friends by creating multiple profiles.\n- Download for Offline Viewing: Watch your favorite shows and movies anytime, anywhere by downloading them.\n- Set Up Parental Controls: Ensure a safe viewing experience for kids with our parental control options.\n\nIf you have any questions or need assistance, our customer support team is here for you 24/7. Visit our Help Center or contact us directly at dmadhusudhan98@gmail.com or 6309792221.\n\nHappy Watching!\n\nBest regards,\nThe Madhusudhan Team\n\nFollow us on ${socialmedia}\n${website}`,
+      `<h1>Welcome to ${name}!</h1>
+      <p>Dear ${name},</p>
+      <p>We are thrilled to have you as part of our community. Whether you are a movie buff, a TV series enthusiast, or just looking for something new to watch, we've got you covered.</p>
+      <p>If you have any questions or need assistance, our customer support team is here for you 24/7. Visit our Help Center or contact us directly at <a href="mailto:dmadhusudhan98@gmail.com">dmadhusudhan98@gmail.com</a> or call us at 6309792221.</p>
+      <p>Happy Watching!</p>
+      <p>Best regards,<br>The Madhusudhan Team</p>
+      <p>Follow us on <a href="${socialmedia}">LinkedIn</a></p>
+      <p><a href="${website}">${website}</a></p>`
     );
 
     res.status(200).json({ message: 'Registration successful' });
@@ -103,13 +111,13 @@ const forgotPassword = async (req, res) => {
     }
 
     const resetToken = crypto.randomBytes(20).toString('hex');
-    const resetTokenExpiry = Date.now() + 3600000; // Token expires in 1 hour
+    const resetTokenExpiry = Date.now() + 600000; // 10 minutes in milliseconds
 
     user.resetPasswordToken = resetToken;
     user.resetPasswordExpires = resetTokenExpiry;
     await user.save();
 
-    const resetUrl = `http://localhost:3000/resetpassword/${resetToken}`;
+    const resetUrl = `${process.env.Rest_URL}${resetToken}`;
     
     await sendEmail(
       user.email,
@@ -121,7 +129,8 @@ const forgotPassword = async (req, res) => {
       `<p>You are receiving this because you (or someone else) have requested the reset of the password for your account.</p>
       <p>Please click on the following link, or paste this into your browser to complete the process:</p>
       <a href="${resetUrl}">${resetUrl}</a>
-      <p>If you did not request this, please ignore this email and your password will remain unchanged.</p>`
+      <p>If you did not request this, please ignore this email and your password will remain unchanged.</p>
+      <p>Note: this link is valid upto 10min only soo hurry up </p>`
     );
 
     res.status(200).json({ status: "success", message: 'Password reset email sent' });
